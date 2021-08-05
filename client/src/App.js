e07b4a67d1938afa5ae36eacf2f32149aa5b24c3
jinang-shah <sjinang@gmail.com>
2021-08-05 18:05:01 +0530

    complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PrivateRoute from "./Components/routings/PrivateRoute";
+
+import PrivateScreen from './Components/screens/PrivateScreen'
+import LoginScreen from './Components/screens/LoginScreen'
+import RegisterScreen from './Components/screens/RegisterScreen'
+import ForgotPasswordScreen from './Components/screens/ForgotPasswordScreen'
+import ResetPasswordScreen from './Components/screens/ResetPasswordScreen'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <PrivateRoute exact path="/" component={PrivateScreen} />
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/register" component={RegisterScreen} />
+          <Route
+            exact
+            path="/forgotpassword"
+            component={ForgotPasswordScreen}
+          />
+          <Route
+            exact
+            path="/passwordreset/:resetToken"
+            component={ResetPasswordScreen}
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,7 @@
+const crypto = require("crypto");
 const User = require("../models/User");
+const ErrorResponse = require("../utils/errorResponse");
+const sendEmail = require("../utils/sendEmail");
 
 exports.register = async (req, res, next) => {
   const { username, password, email } = req.body;
@@ -9,15 +12,10 @@ exports.register = async (req, res, next) => {
       email,
       password,
     });
-    res.status(201).json({
-      success: true,
-      user,
-    });
+
+    sendToken(user, 201, res);
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: error.message,
-    });
+    next(error);
   }
 };
 
@@ -25,15 +23,13 @@ exports.login = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    res
-      .status(400)
-      .json({ success: false, error: "please provide email and password" });
+    return next(new ErrorResponse("please provide email and password", 401));
   }
 
   try {
     const user = await User.findOne({ email }).select("+password");
     if (!user) {
-      res.status(404).json({ success: false, error: "Invalid credentials" });
+      return next(new ErrorResponse("Invalid credentials", 401));
     }
 
     const isMatch = await user.matchPassword(password);
@@ -41,22 +37,85 @@ exports.login = async (req, res, next) => {
     if (!isMatch) {
       res.status(404).json({ success: false, error: "Invalid credentials" });
     }
-    res.status(200).json({
-      success: true,
-      token: "flvfvsfvcakc",
-    });
+
+    sendToken(user, 200, res);
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: error.message,
-    });
+    next(error);
   }
 };
 
-exports.forgotpassword = (req, res, next) => {
-  res.send("Forgot Password Route");
+exports.forgotpassword = async (req, res, next) => {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return next(new ErrorResponse("Email could not be sent, no user", 404));
+    }
+
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save();
+
+    const resetUrl = `http://localhost:3000/passwordreset/${resetToken}`;
+
+    const message = `
+      <h1>You have requested a password reset</h1>
+      <p>please go to this link to reset your password</p>
+      <a href = ${resetUrl} clicktracking=off>${resetUrl}</a>
+    `;
+
+    try {
+      await sendEmail({
+        to: user.email,
+        subject: "password reset request",
+        text: message,
+      });
+      res.status(200).json({ success: true, data: "Email sent" });
+    } catch (error) {
+      user.resetpasswordToken = undefined;
+      user.resetpasswordExpire = undefined;
+
+      await user.save();
+
+      return next(new ErrorResponse("Email could not be sent !", 500));
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
-exports.resetpassword = (req, res, next) => {
-  res.send("Reset Password Route");
+exports.resetpassword = async (req, res, next) => {
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.resetToken)
+    .digest("hex");
+  try {
+    const user = await User.findOne({
+      resetPasswordToken,
+      resetPasswordExpire: { $gt: Date.now() },
+    });
+    if (!user) {
+      return next(new ErrorResponse("Invalid reset Token", 400));
+    }
+
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    res.status(201).json({
+      success: true,
+      data: "password reset success",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const sendToken = (user, statusCode, res) => {
+  const token = user.getSignedToken();
+  res.status(statusCode).json({ success: true, token });
 };
